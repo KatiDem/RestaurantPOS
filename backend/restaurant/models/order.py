cec0e8a4f23c6cf100222462625f36270ef2f53e
Katerina <you@a.com>
2021-04-12 20:18:59 +0300

    add model order, serializator for order and views

---
+++ backend/restaurant/models/order.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta, date
+
+
+class Order(models.Model):
+
+    # payment = models.ForeignKey(Payment, on_delete=models.CASCADE, blank=True, null=True)  # order of payment
+    # table = models.ForeignKey(Seating, on_delete=models.CASCADE)
+    time = models.DateTimeField()  # The time at which the order was taken
+    # items = models.ManyToManyField(OrderItem)
+    cooking_instructions = models.CharField(max_length=500, default='na')  # Preferences, allergies, etc.
+    purchase_method = models.CharField(max_length=100, default='na')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    confirmed = models.BooleanField(default=False)  # order has been confirmed
+    cancelled = models.BooleanField(default=False)
+    ready_delivery = models.BooleanField(default=False)  # order is ready for delivery
+    delivered = models.BooleanField(default=False)  # order has been delivered
+    delayed = models.BooleanField(default=False)  # order is delayed
+    paid = models.BooleanField(default=False)  # order has been paid
--- backend/restaurant/views.py
+++ backend/restaurant/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework.generics import *
+from rest_framework.filters import *
+from .models import Order
+from .serializers import OrderSerializer
 
-# Create your views here.
+
+class OrderCreateView(CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderListView(ListAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+class OrderDestroyView(DestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
